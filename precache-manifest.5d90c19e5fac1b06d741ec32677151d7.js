44aff75509218dac93b7e866af86b2417cc2850e
Armando Hinojosa Cc <sinpatrones1@gmail.com>
2020-04-27 01:45:42 -0500

    Updates

---
+++ precache-manifest.5d90c19e5fac1b06d741ec32677151d7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9b8450073bdf13daae4b17462e96979",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7388d28924a537d3b75c",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "0f13aea0c0bc58341d6f",
+    "url": "/static/css/main.523f09c5.chunk.css"
+  },
+  {
+    "revision": "7388d28924a537d3b75c",
+    "url": "/static/js/2.c820c0c7.chunk.js"
+  },
+  {
+    "revision": "8512134e8f78277a1e3b060fcac52421",
+    "url": "/static/js/2.c820c0c7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0f13aea0c0bc58341d6f",
+    "url": "/static/js/main.f940adae.chunk.js"
+  },
+  {
+    "revision": "0eecef6a44e56b3cb0d6",
+    "url": "/static/js/runtime-main.613173f2.js"
+  }
+]);
